f826f14611648921aa86d0aca198915424793b6d
jfms <jfmserrano@gmail.com>
2012-02-18 12:20:44 +0100

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,12 @@ describe UsersController do
 
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the righr title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign Up")
     end   
   
@@ -46,5 +46,33 @@ describe UsersController do
        response.should have_selector("h1>img", :class => "gravatar")
      end
   end
+
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+  end
+
 end
 
