a0418435c28be7793da8a3e74dcd1fcb6d0fe65f
jfms <jfmserrano@gmail.com>
2012-03-10 18:07:56 +0100

    Exercises Lesson10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   before_filter :correct_user, :only => [ :edit, :update]
   before_filter :admin_user,   :only => :destroy
 
+# Signed-in users have no reason to access the new and create actions
+  before_filter :new_user, :only => [ :new, :create]
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed"
@@ -71,7 +74,11 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_path) unless current_user.admin? 
+    end
+
+    def new_user
+      redirect_to(root_path) unless !signed_in?
     end
 
 end
